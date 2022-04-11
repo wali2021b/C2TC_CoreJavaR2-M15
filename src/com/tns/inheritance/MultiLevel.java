c6633baf3d979a5a9e45f02b10ef6b982bbf56f2
Sayyed Waliullah <jiesm2020@gmail.com>
2022-04-11 23:24:56 +0530

    First Commit..

--- src/com/tns/inheritance/MultiLevel.java
+++ src/com/tns/inheritance/MultiLevel.java
@@ -11,19 +11,36 @@ class Person{
 }
 class Emp extends Person{
 	String Work_palce="Mumbai";
+	String name="ABC";
 	Emp()
 	{
 		System.out.println("Employee constructor..");
 	}
 	void place()
 	{
-		System.out.println("Working in "+ Work_palce);
+		System.out.println(name+" Working in "+ Work_palce);
+	}
+	
+	class Programmar extends Emp{
+		
+		Programmar()
+		{
+		  System.out.println("Programmar Constructor Or the Grand Son class..");
+		}
+		void code()
+		{
+			System.out.println(name+" Is a good Coder and Programmar of the company..");
+		}
 	}
 	
 }
 public class MultiLevel {
 
 	public static void main(String[] args) {
+//		Programmar p1= new Programmar();
+//	Programmar pp= new Programmar();
+		Emp e1=new Emp();
+		e1.place();
 
 	}
 
