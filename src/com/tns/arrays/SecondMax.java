135f6fb64cb07ce15eca1f8a54a01e406358c034
Sayyed Waliullah <jiesm2020@gmail.com>
2022-05-10 18:21:26 +0530

    First Commit..

---
+++ src/com/tns/arrays/SecondMax.java
@@ -0,0 +1,10 @@
+package com.tns.arrays;
+
+public class SecondMax {
+
+	public static void main(String[] args) {
+
+		System.out.println("Implement an array program to find second Max in the array..");
+	}
+
+}
